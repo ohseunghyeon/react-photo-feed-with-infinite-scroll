48f60e86eff1721e942c8ac155ea30fc9179acc9
ohseunghyeon <postercolo@gmail.com>
2019-07-25 12:06:14 +0900

    Updates

---
+++ precache-manifest.d4b3025e3566f1e7d1acbcba6d1e2560.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3139af841425721eb1557d24fb65e4c8",
+    "url": "/react-photo-feed-with-infinite-scroll/index.html"
+  },
+  {
+    "revision": "715f5261938d13104ba2",
+    "url": "/react-photo-feed-with-infinite-scroll/static/css/main.14b92a9d.chunk.css"
+  },
+  {
+    "revision": "fc6e79ebf11723072d19",
+    "url": "/react-photo-feed-with-infinite-scroll/static/js/2.267189ff.chunk.js"
+  },
+  {
+    "revision": "715f5261938d13104ba2",
+    "url": "/react-photo-feed-with-infinite-scroll/static/js/main.8a1b6ed5.chunk.js"
+  },
+  {
+    "revision": "7b887743f20e09ccd47e",
+    "url": "/react-photo-feed-with-infinite-scroll/static/js/runtime~main.19659c6d.js"
+  },
+  {
+    "revision": "8753c397492b363d55424cb8797af355",
+    "url": "/react-photo-feed-with-infinite-scroll/static/media/checkbox_checked.8753c397.svg"
+  },
+  {
+    "revision": "9efa55affa87d9742216a58c63366f1b",
+    "url": "/react-photo-feed-with-infinite-scroll/static/media/checkbox_unchecked.9efa55af.svg"
+  },
+  {
+    "revision": "0c7e34a27d8dc18c0de50a7c2ddb6c9f",
+    "url": "/react-photo-feed-with-infinite-scroll/static/media/scrap_off.0c7e34a2.svg"
+  },
+  {
+    "revision": "be2a9f763992cf70b683f10ee126a2c7",
+    "url": "/react-photo-feed-with-infinite-scroll/static/media/scrap_on.be2a9f76.svg"
+  }
+]);
--- src/hooks/index.js
+++
@@ -1,2 +0,0 @@
-export { default as useScrapList } from './useScrapList';
-export { default as useFeeds } from './useFeeds';
